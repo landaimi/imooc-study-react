9c1b9d2facc49f90b3d248495b7935e9d3e6cdb8
zhangff <zhangff@wanhuhealth.com>
2018-07-30 17:35:56 +0800

    gallery-by-react finished

--- gallery-by-react/src/components/Figures.js
+++ gallery-by-react/src/components/Figures.js
@@ -1,13 +1,56 @@
-import React, { Component } from 'react';
-import image from '../images/1.jpg';
+import React, { Component } from 'react'; 
+
 class Figures extends Component {
+     /*
+     * imgFigure 的点击处理函数
+     */
+    handleClick = function(e) {
+        if (this.props.arrange.isCenter) {
+          this.props.inverse();
+        } else {
+          this.props.center();
+        }
+  
+        e.stopPropagation();
+        e.preventDefault();
+    }
     render(){
-        return (<div className='imgSec'>
-            <figure >
-                <p>{this.props.image.title}</p>
-                <img src={this.props.image.imageURL}></img>
+        console.log(this.props);
+        var styleObj = {};
+        // 如果props属性中指定了这张图片的位置，则使用
+        if (this.props.arrange.pos) {
+            styleObj = this.props.arrange.pos;
+        }
+
+        // 如果图片的旋转角度有值并且不为0， 添加旋转角度
+        if (this.props.arrange.rotate) {
+          (['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach(function (value) {
+            styleObj[value] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+          }.bind(this));
+        }
+        
+        // 如果是居中的图片， z-index设为11
+        if (this.props.arrange.isCenter) {
+          styleObj.zIndex = 11;
+        }
+
+        var imgFigureClassName = 'img-figure';
+        imgFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
+        return (
+            <figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick}>
+                <img src={this.props.data.imageURL}
+                     alt={this.props.data.title}
+                />
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                    <div className="img-back" onClick={this.handleClick}>
+                      <p>
+                        {this.props.data.desc}
+                      </p>
+                    </div>
+                </figcaption>
             </figure>
-        </div>)
+        )
     }
 }
 
